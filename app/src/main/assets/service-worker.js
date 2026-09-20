88f8829664cb9e694f88b53d5270b1f86d7e7564
alextelles0023-beep <alextelles0023-beep@users.noreply.github.com>
2026-09-20 07:30:08 -0300

    Xandy Prompt Studio 3.0 - 200 personagens e Deuses 3C

--- app/src/main/assets/service-worker.js
+++ app/src/main/assets/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='xandy-studio-v2';
-const ASSETS=['./','./index.html','./estilos.html','./3camadas.html','./deuses.html','./personagens.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
+const CACHE='xandy-studio-v3';
+const ASSETS=['./','./index.html','./estilos.html','./deuses.html','./personagens.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>caches.match('./index.html'))))});
